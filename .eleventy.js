7a90cff0df8d6082188a23f6a61be3dfdaa28ca6
omerovi5 <123518803+omerovi5@users.noreply.github.com>
2026-03-02 03:50:14 +0000

    try again

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
 
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
+
   eleventyConfig.addPassthroughCopy("styles");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
 };
 
 module.exports = function(eleventyConfig) {
