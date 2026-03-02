589d5f30b3a781fff0b805d28954f012d505d879
omerovi5 <123518803+omerovi5@users.noreply.github.com>
2026-03-02 03:14:26 +0000

    css/shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -4,3 +4,17 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addShortcode("transmission", function(title, text) {
+    return `
+      <div class="transmission">
+        <h3>📡 ${title}</h3>
+        <p>${text}</p>
+      </div>
+    `;
+  });
+
+  eleventyConfig.addPassthroughCopy("css");
+};
